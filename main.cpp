7bcf56cf14041457d483ee2ac0ad6b139386e2a6
Andrew <emaillyonam@gmail.com>
2023-09-29 00:52:25 -0400

    Update main.cpp
    Making progress, renders window with grid with values and values evolves but not properly deleting dead cells.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,185 @@
 #include <vector>
 #include <tuple>
 #include <array>
+#include <unordered_map>
+#include <set>
+//#include "next_generation.h"
 
 const int WIDTH = 1000, HEIGHT = 1000;
+
+namespace std {
+    template <>
+    struct hash<std::tuple<int,int>> {
+        size_t operator()(const std::tuple<int,int>& t) const {
+            return std::hash<int>()(std::get<0>(t)) ^ std::hash<int>()(std::get<1>(t));
+        }
+    };
+}
+
+std::vector<std::tuple<int,int>> next_generation(const std::vector<std::tuple<int,int>> &old_Life){
+
+
+    std::unordered_map<std::tuple<int, int>, int> new_life_map;
+    std::vector<std::tuple<int,int>> new_life;
+
+    for (const auto& tuple : old_Life){
+        int x_pos = std::get<0>(tuple);
+        int y_pos = std::get<1>(tuple);
+        std::cout << "x_pos is " << x_pos << ", y_pos is " << y_pos << std::endl;
+    
+        // top left
+        std::tuple<int, int> new_tuple = std::make_tuple(x_pos - 20, y_pos + 20);
+        // std::tuple<int, int> new_tuple(x_pos, y_pos);  Could also do it this way
+        int current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // top
+        std::get<0>(new_tuple) = x_pos;
+        std::get<1>(new_tuple) = y_pos + 20;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // top right
+        std::get<0>(new_tuple) = x_pos + 20;
+        std::get<1>(new_tuple) = y_pos + 20;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // left
+        std::get<0>(new_tuple) = x_pos - 20;
+        std::get<1>(new_tuple) = y_pos;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // right
+        std::get<0>(new_tuple) = x_pos + 20;
+        std::get<1>(new_tuple) = y_pos;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // bot left
+        std::get<0>(new_tuple) = x_pos - 20;
+        std::get<1>(new_tuple) = y_pos - 20;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // bot
+        std::get<0>(new_tuple) = x_pos;
+        std::get<1>(new_tuple) = y_pos - 20;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+
+        // bot right
+        std::get<0>(new_tuple) = x_pos + 20;
+        std::get<1>(new_tuple) = y_pos - 20;
+        current_value = new_life_map[new_tuple];
+        if (current_value != 0){
+            new_life_map[new_tuple]++;
+        }
+        else {
+            new_life_map[new_tuple] = 1;
+        }
+    }
+    // Iterate through all the keys looking for values that are equal to 3, indicating a new live square
+    for (const auto& pair : new_life_map) {
+        const std::tuple<int, int>& key = pair.first;
+        const int val = pair.second;
+        if (val == 3){
+            new_life.push_back(pair.first);
+        }
+    }
+    // check neighbors of living cells
+    for (const auto& cell : old_Life){
+        int nCount = 0;
+        int x_cell = std::get<0>(cell);
+        int y_cell = std::get<1>(cell);
+        for(const auto& sub_cell : old_Life){
+            if (cell != sub_cell){
+                // top left
+                if (x_cell - 20 == std::get<0>(sub_cell) && y_cell + 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // top
+                if (x_cell == std::get<0>(sub_cell) && y_cell + 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // top right
+                if (x_cell + 20 == std::get<0>(sub_cell) && y_cell + 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // left
+                if (x_cell - 20 == std::get<0>(sub_cell) && y_cell == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // right
+                if (x_cell + 20 == std::get<0>(sub_cell) && y_cell == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // bot left
+                if (x_cell - 20 == std::get<0>(sub_cell) && y_cell - 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // bot
+                if (x_cell == std::get<0>(sub_cell) && y_cell - 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+                // bot right
+                if (x_cell + 20 == std::get<0>(sub_cell) && y_cell - 20 == std::get<1>(sub_cell)){
+                    nCount++;
+                }
+            }
+        }
+        if (nCount == 2 || nCount == 3){
+            new_life.push_back(cell);
+        }
+    }
+    // Remove duplicates
+    std::set<std::tuple<int,int>> no_duplicates;
+    for (std::tuple<int, int>& element : new_life) {
+        no_duplicates.insert(element);
+    }
+    new_life.clear();
+    for (const std::tuple<int, int>& unique_element : no_duplicates){
+        new_life.push_back(unique_element);
+    }
+
+    return new_life;
+}
 
 std::vector<std::tuple<int,int>> gosper_gun_builder() {
     
@@ -85,6 +262,8 @@ int main(int argc, char* argv[])
             SDL_RenderDrawLine(renderer, 0, y, WIDTH, y);
         }
         
+        life_list = next_generation(life_list);
+
         for (const auto& tuple : life_list){
             int x_pos = std::get<0>(tuple);
             int y_pos = std::get<1>(tuple);
